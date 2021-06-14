74d2edd4b6dc737564bfc471e2b5b244ec24042d
Erik Svensson <eosvensson@gmail.com>
2021-06-14 09:51:57 +0200

    Added IOC for dao package

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,10 +1,25 @@
 package se.lexicon;
 
 
-public class App 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.lexicon.dao.interfaces.PersonDAO;
+import se.lexicon.model.entity.Person;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        PersonDAO personDAO = context.getBean(PersonDAO.class);
+
+        System.out.println(personDAO.persist(new Person(
+                1, "Erik", "Svensson", "erik.svensson@lexicon.se", null
+        )));
+
+
+
+        context.close();
     }
 }
